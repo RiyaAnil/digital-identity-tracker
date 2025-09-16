efe90569a115274181caf9b47822251c468874b1
Riya <riyaanilt@gmail.com>
2025-09-16 19:09:28 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-red-600">
+        Hello Vite + React + Tailwind!
+      </h1>
+    </div>
+  )
+}
+
+export default App
