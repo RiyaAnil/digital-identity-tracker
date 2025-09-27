0ffee4b8c73458e1b8b0f6ba0c3c92ed3af03c58
unknown <nandanasreeja26@gmail.com>
2025-09-27 15:33:52 +0530

    updated requirements

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 load_dotenv()  # Load variables from .env
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+print("DATABASE_URL =", os.getenv("DATABASE_URL"))
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
