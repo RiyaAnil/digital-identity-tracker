5ab1c3bce51ac48cbcffdda880ff20d6b62ba1aa
unknown <nandanasreeja26@gmail.com>
2025-09-27 15:15:34 +0530

    Added .env

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,17 +1,24 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:digidtra123@db.lbpuzkqbarlbfjqvlzbd.supabase.co:5432/postgres"
+load_dotenv()  # Load variables from .env
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Dependency
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+if __name__ == "__main__":
+    from app.models import account, data_type  # import your models
+    Base.metadata.create_all(bind=engine)
+    print("Tables created successfully!")
