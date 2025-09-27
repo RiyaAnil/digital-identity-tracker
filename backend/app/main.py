aacf4c23dd45bf7038971954e96ee050afe174bd
unknown <nandanasreeja26@gmail.com>
2025-09-27 14:54:40 +0530

    Updated main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from app.models import account, data_type  # import your new models
-
+from app.routers import accounts, data_types
 
 app = FastAPI()
+# Register routers
+app.include_router(accounts.router)
+app.include_router(data_types.router)
 
 @app.get("/")
 def read_root():
